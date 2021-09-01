04f50dc7b08d061d34e2bdb5d9992367959c76bf
MaikeRM <maikemota@live.com>
2021-09-01 19:52:53 -0300

    mrmota

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ import turtlesystem as TurtleSystem
 from functions import getStratStats
 
 # Sample 10 tickers from S&P 500
-url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+url = 'https://pt.wikipedia.org/wiki/Lista_de_companhias_citadas_no_Ibovespa'
 table = pd.read_html(url)
 df = table[0]
-syms = df['Symbol']
+syms = np.empty(10, dtype = object)
+i=0
+for acao in df['Código']:
+    syms[i] = (acao+'.SA')
+    i+=1
+    if i == 10: break
+#syms = np.asarray(syms)
 # Sample symbols
-tickers = list(np.random.choice(syms.values, size=10))
+tickers = list(np.random.choice(syms, size=10))
+#tickers = ['PETR4.SA', 'VALE3.SA', 'ITUB4.SA', 'BBDC4.SA', 'JBSS3.SA', 'FLRY3.SA', 'ABEV3.SA']
 print("Ticker Symbols:")
 _ = [print(f"\t{i}") for i in tickers]
-sys = TurtleSystem(tickers, init_account_size=1E4, start='2000-01-01')
+sys = TurtleSystem(tickers, init_account_size=1E4, start='2013-01-01')
 sys.run()
-
 
 port_values = sys.get_portfolio_values()
 returns = port_values / port_values.shift(1)
@@ -25,23 +31,23 @@ log_returns = np.log(returns)
 cum_rets = log_returns.cumsum()
 
 # Compare to SPY baseline
-sp500 = yf.Ticker('SPY').history(start=sys.start, end=sys.end)
-sp500['returns'] = sp500['Close'] / sp500['Close'].shift(1)
-sp500['log_returns'] = np.log(sp500['returns'])
-sp500['cum_rets'] = sp500['log_returns'].cumsum()
+BVSP = yf.Ticker('^BVSP').history(start=sys.start, end=sys.end)
+BVSP['returns'] = BVSP['Close'] / BVSP['Close'].shift(1)
+BVSP['log_returns'] = np.log(BVSP['returns'])
+BVSP['cum_rets'] = BVSP['log_returns'].cumsum()
 
 plt.figure(figsize=(12, 8))
 plt.plot((np.exp(cum_rets) -1 )* 100, label='Turtle Strategy')
-plt.plot((np.exp(sp500['cum_rets']) - 1) * 100, label='SPY')
-plt.xlabel('Date')
-plt.ylabel('Returns (%)')
-plt.title('Cumulative Portfolio Returns')
+plt.plot((np.exp(BVSP['cum_rets']) - 1) * 100, label='BVSP')
+plt.xlabel('Data')
+plt.ylabel('Returno (%)')
+plt.title('Retorno acumulado do portifólio')
 plt.legend()
 plt.tight_layout()
 plt.show()
 
 stats = getStratStats(log_returns)
-spy_stats = getStratStats(sp500['log_returns'])
+bvsp_stats = getStratStats(BVSP['log_returns'])
 df_stats = pd.DataFrame(stats, index=['Turtle'])
-df_stats = pd.concat([df_stats, pd.DataFrame(spy_stats, index=['SPY'])])
+df_stats = pd.concat([df_stats, pd.DataFrame(bvsp_stats, index=['BVSP'])])
 df_stats
